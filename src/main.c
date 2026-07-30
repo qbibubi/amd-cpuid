9220dba1bb7f4c48efa8b00db3494a7d5026cbb2
qbibubi <58397100+qbibubi@users.noreply.github.com>
2026-07-30 11:58:57 +0200

    fix: includes in main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,4 @@
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-
-#ifdef _WIN32
-# include "amd/windows.h"
-#else
-# include "amd/linux.h"
-#endif // _WIN32
+#include "amd/cpuid.h"
 
 int main() {
   cpuid_ctx ctx = { 0 };
